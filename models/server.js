0a0b0a0e3c063a6a1276885e43cebdf3b82ddd83
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-12-31 21:59:46 -0300

    server model added

---
+++ models/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 8080;
+    this.path = {
+      
+    };
+
+    //base de datos
+    this.connectionDB();
+    //middlewares
+    this.middlewares();
+    //rutas de mi app
+    this.routes();
+  }
+
+  async connectionDB() {
+  }
+
+  middlewares() {
+    //cors
+    this.app.use(cors());
+
+    //lectura y parseo del body
+    this.app.use(express.json());
+
+    //directorio publico
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("servidor corriendo en puerto", this.port);
+    });
+  }
+}
+
+module.exports = Server;

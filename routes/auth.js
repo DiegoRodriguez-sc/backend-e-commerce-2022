984c292df0fadcf61dd15ea4a622ab4cfc3838fb
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-05 17:52:14 -0300

    f

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,36 @@
-
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { login, register, revalidarTWJ } = require("../controller/auth.controller");
+const {
+  login,
+  register,
+  revalidarTWJ,
+} = require("../controller/auth.controller");
 const { validateData } = require("../middlewares/validateData");
 const { validateJwt } = require("../middlewares/validateJwt");
 
 const router = Router();
 
-router.post("/login",[
-  check("email","El correo es obligatorio").isEmail(),
-  check("password","La contraseña es obligatoria").not().isEmpty(),
-  validateData
-],login);
+router.post(
+  "/login",
+  [
+    check("email", "El correo es obligatorio").isEmail(),
+    check("password", "La contraseña es obligatoria").not().isEmpty(),
+    validateData,
+  ],
+  login
+);
 
+router.post(
+  "/register",
+  [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El correo es obligatorio").isEmail(),
+    check("password", "La contraseña es obligatoria").not().isEmpty(),
+    validateData,
+  ],
+  register
+);
 
-router.post("/register",[
-  check("name","El nombre es obligatorio").not().isEmpty(),
-  check("email","El correo es obligatorio").isEmail(),
-  check("password","La contraseña es obligatoria").not().isEmpty(),
-  validateData
-],register);
-
-router.get("/renew",[
-  validateJwt
-],revalidarTWJ);
+router.get("/renew", [validateJwt], revalidarTWJ);
 
 module.exports = router;
